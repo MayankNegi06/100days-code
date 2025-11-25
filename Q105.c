58e164992859fdfcd789ec73518894e633c5302b
MayankNegi06 <Mayank.26399@stu.upes.ac.in>
2025-11-25 11:15:42 +0530

    Add program to find first greater element to the left
    Implement a program that reads an array and prints the first greater element to the left for each element.

---
+++ Q105.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    for(int i = 0; i < n; i++) {
+        int found = 0;
+        for(int j = i - 1; j >= 0; j--) {
+            if(arr[j] > arr[i]) {
+                if(i > 0) printf(",");
+                printf("%d", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if(!found) {
+            if(i > 0) printf(",");
+            printf("-1");
+        }
+    }
+    printf("\n");
+    return 0;
+}
